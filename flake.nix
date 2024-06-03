fa73119e5804db0861ae2d31f3ef7e2e71cd3abd
acrease <alex@crease.sh>
2024-06-03 22:22:14 +0100

    test

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
   outputs = { self, nixpkgs, home-manager, hyprland, hyprlock, nur, ... }@inputs: {
