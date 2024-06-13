9695c70026f234c1eb76f15ef777f01e2e113802
acrease <alex@crease.sh>
2024-06-13 14:56:32 +0100

    Automatic commit on exp branch

--- modules/home-manager/hyprpaper.nix
+++ modules/home-manager/hyprpaper.nix
@@ -7,7 +7,7 @@
       ipc = "on";
       preload = [
         "/home/acrease/Documents/homelab/assets/dark_nature.jpg"
-      ]
+      ];
       wallpaper = [
         "HDMI-A-1, /home/acrease/Documents/homelab/assets/dark_nature.jpg"
         "DP-1, /home/acrease/Documents/homelab/assets/dark_nature.jpg"
