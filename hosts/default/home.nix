84ba7798c0000353fac6779dcb9f31f9b6d2272e
acrease <alex@crease.sh>
2024-06-14 00:18:17 +0100

    Automatic commit on exp branch

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -150,30 +150,7 @@ in
     "utils.py" = {
       enable = true;
       executable = true;
-      text = ''
-        \# !/usr/bin/env python3
-
-        import os
-        import subprocess
-
-        def init_keys():
-            keys = ["bitbucket", "github", "salas"]
-            key_dir = os.path.expanduser("~/.keys")
-
-            # Create the directory if it doesn't exist
-            os.makedirs(key_dir, exist_ok=True)
-
-            for key in keys:
-                key_path = os.path.join(key_dir, key)
-
-                if not os.path.exists(key_path):
-                    subprocess.run(["ssh-keygen", "-t", "rsa", "-b", "4096", "-N", "", "-f", key_path])
-                else:
-                    print(f"\{key\} key already exists.")
-
-        if __name__ == "__main__":
-            init_keys()
-      '';
+      source = "./assets/utils.py"
     };
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
