e6c3890eeb6a053e7f67e5c0185beaeadb523bff
acrease <alex@crease.sh>
2024-06-14 00:18:24 +0100

    Automatic commit on exp branch

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -150,7 +150,7 @@ in
     "utils.py" = {
       enable = true;
       executable = true;
-      source = "./assets/utils.py"
+      source = "./assets/utils.py";
     };
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
