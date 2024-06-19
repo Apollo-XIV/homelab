b039feb5b91458edd378e1fbb01998532f204b35
acrease <alex@crease.sh>
2024-06-19 21:54:58 +0100

    fix typo

--- hosts/salas/configuration.nix
+++ hosts/salas/configuration.nix
@@ -202,7 +202,7 @@
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
-  }
+  };
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
