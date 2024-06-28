4f1d903be663f1ece80c0e7aaa86cf0bc1d1f271
acrease <alex@crease.sh>
2024-06-28 21:11:25 +0100

    Automatic commit on exp branch

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,7 +1,47 @@
-provider "kubernetes" {
-
+variable "salas_key" {
+  type    = string
+  default = ""
 }
 
-provider "helm" {
+# resource "terraform_data" "ssh_test" {
+#   connection {
+#     host        = "salas"
+#     private_key = local.key
+#     user        = "acrease"
+#   }
+#   provisioner "remote-exec" {
+#     inline = ["touch test"]
+#   }
+# }
 
+resource "terraform_data" "config_repo" {
+  connection {
+    host        = "salas"
+    private_key = try(file("~/.ssh/salas"), file("${path.root}/salas_key"))
+    user        = "acrease"
+  }
+  provisioner "remote-exec" {
+    when   = create
+    inline = ["git clone https://github.com/apollo-xiv/homelab config"]
+  }
+
+  provisioner "remote-exec" {
+    when   = destroy
+    inline = ["rm -rf config"]
+  }
+}
+
+resource "null_resource" "update" {
+  connection {
+    host        = "salas"
+    private_key = try(file("~/.ssh/salas"), file("${path.root}/salas_key"))
+    user        = "root"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "git -C ~/config pull",                     // pull the latest config
+      "nixos-rebuild test --flake ~/config#salas" // sudo nixos update
+    ]
+  }
+  depends_on = [terraform_data.config_repo]
 }
