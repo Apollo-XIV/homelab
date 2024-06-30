d9c66b156a01367e8e4f7ae200706efe027ac9ab
acrease <alex@crease.sh>
2024-06-30 23:52:39 +0100

    Automatic commit on exp branch

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -56,6 +56,8 @@
   user.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.trusted-substituters = ["https://ai.cachix.org"];
+  nix.settings.trusted-public-keys = ["ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="];
   # Enable networking
   networking.networkmanager.enable = true;
 
