5a0fb1326b3e561de7415ae2fc56d3c002bf79d5
acrease <alex@crease.sh>
2024-07-19 14:57:54 +0100

    Automatic commit on exp branch

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -264,6 +264,8 @@
     enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
+
+  services.sunshine.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
