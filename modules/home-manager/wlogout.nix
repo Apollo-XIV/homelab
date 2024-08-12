fa40e84702042c7b94869cc2ecd93826be78d31a
acrease <alex@crease.sh>
2024-08-12 20:34:40 +0100

    Automatic commit on exp branch

--- modules/home-manager/wlogout.nix
+++ modules/home-manager/wlogout.nix
@@ -5,13 +5,31 @@
 		enable = true;
 		layout = [
 			{
-				label = "shutdown";
-				action = "";
-				text = "";
+				label = "lock";
+				action = "sleep 1; hyprlock";
+				text = "lock";
 				circular = true;
 				keybind = "";
-				height = 1;
-				width = 1;
+				height = 0.5;
+				width = 0.5;
+			}
+			{
+				label = "logout";
+				action = "sleep 1; hyprctl dispatch exit";
+				text = "logout";
+				circular = true;
+				keybind = "";
+				height = 0.5;
+				width = 0.5;
+			}
+			{
+				label = "shutdown";
+				action = "systemctl poweroff";
+				text = "shutdown";
+				circular = true;
+				keybind = "";
+				height = 0.5;
+				width = 0.5;
 			}
 		];
 	};
