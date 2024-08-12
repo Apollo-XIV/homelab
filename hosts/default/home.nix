d12278d0e921e069462d9d94e0fcb92ca3b80730
acrease <alex@crease.sh>
2024-08-12 23:09:56 +0100

    Automatic commit on exp branch

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -167,6 +167,11 @@ in
     };
   };
 
+  stylix = {
+    enable = true;
+    autoEnable = true;
+  };
+
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. If you don't want to manage your shell through Home
   # Manager then you have to manually source 'hm-session-vars.sh' located at
