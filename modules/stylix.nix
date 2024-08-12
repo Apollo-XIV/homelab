d15969847e5028de1929a68e2c68efaa7f9d7741
acrease <alex@crease.sh>
2024-08-12 22:49:08 +0100

    Automatic commit on exp branch

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -5,5 +5,26 @@
     enable = true;
     image = /home/acrease/Documents/homelab/assets/dark_nature.png;
     polarity = "dark";
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      monospace = {
+        package = pkgs.fira-code-nerdfont;
+        name = "FiraCode Nerd Font";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    }
   };
 }
