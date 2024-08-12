6c992ac6df75c2b26b0b5ec234e50dd77ff4192b
acrease <alex@crease.sh>
2024-08-12 20:42:20 +0100

    Automatic commit on exp branch

--- modules/home-manager/hyprlock.nix
+++ modules/home-manager/hyprlock.nix
@@ -21,13 +21,13 @@
         {
           size = "200, 50";
           position = "0, -80";
-          monitor = "";
+          monitor = "HDMI-A-1";
           dots_center = true;
           fade_on_empty = false;
           font_color = "rgb(202, 211, 245)";
           inner_color = "rgb(91, 96, 120)";
           outer_color = "rgb(24, 25, 38)";
-          outline_thickness = 5;
+          outline_thickness = 2;
           placeholder_text = ''<span foreground="##cad3f5">Password...</span>'';
           shadow_passes = 2;
         }
