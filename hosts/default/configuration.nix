91612d1559f756429a851d48e4088e634a78b44c
acrease <alex@crease.sh>
2024-09-02 22:38:36 +0100

    Automatic commit on exp branch

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -2,9 +2,9 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, inputs, pkl, ... }:
+{ config, pkgs, inputs, ... }:
 let
-  pkl-cli = pkl.packages.${pkgs.system}.pkl;
+  pkl-cli = inputs.pkl.packages.${pkgs.system}.pkl;
 in
 {
   imports =
