281344a505748739dfd14a9fb9806341b2b41f24
acrease <alex@crease.sh>
2024-09-30 00:49:32 +0100

    Automatic commit on exp branch

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -343,6 +343,7 @@ in
     enableOnBoot = true;
     autoPrune.enable = true;
     rootless.enable = true;
+    enableNvidia = true;
   };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
